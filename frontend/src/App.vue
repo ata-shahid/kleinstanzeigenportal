c5bff50f57c17f805c0dd7b8490399ada13797ed
Shahid, Ataul <Ataul.Shahid@student.hs-rm.de>
2026-06-22 18:27:11 +0200

    feat(frontend): add header and footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <header class="kopf">
+    <div class="kopf-container">
+      <div class="brandname-container">
+        <h1 class="company-name">Kleinstanzeigen</h1>
+      </div>
+      <div class="navbar">
+        <div class="nav-links">
+          <a class="benutzer-link" href="/admin/benutzer">Benutzer</a>
+          <a class="benutzer-link" href="/admin/anzeige">Anzeige</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main class="main-container">
+    <div></div>
+  </main>
+
+  <footer class="fuss">
+    <div class="fuss-container">
+      <p class="fuss-text">&copy; 2026 Webbasierte Anwendungen. All rights reserved.</p>
+    </div>
+  </footer>
 </template>
 
 <style scoped></style>
